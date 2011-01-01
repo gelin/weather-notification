e943533990bf7eaad5edfbdf1467ee5f5014add2
Denis Nelubin <dnelubin@gmail.com>
2011-01-01 20:02:28 +0600

    Started UI implementation...

--- src/ru/gelin/android/weather/notification/MainActivity.java
+++ src/ru/gelin/android/weather/notification/MainActivity.java
@@ -1,13 +1,14 @@
 package ru.gelin.android.weather.notification;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+public class MainActivity extends PreferenceActivity {
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        addPreferencesFromResource(R.xml.main_preferences);
     }
+
 }
