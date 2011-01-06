ba64fe215b9ff4bd3f4439729b073b943a255445
Denis Nelubin <dnelubin@gmail.com>
2011-01-07 01:36:54 +0600

    Added progress icon and update after location change.

--- src/ru/gelin/android/weather/notification/MainActivity.java
+++ src/ru/gelin/android/weather/notification/MainActivity.java
@@ -1,14 +1,62 @@
 package ru.gelin.android.weather.notification;
 
+import static ru.gelin.android.weather.notification.WeatherStorage.WEATHER;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.view.Window;
 
-public class MainActivity extends PreferenceActivity {
+public class MainActivity extends PreferenceActivity 
+        implements OnPreferenceClickListener, OnPreferenceChangeListener {
+
+    /** Manual location preference name */
+    static final String LOCATION = "location";
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);    //before super()!
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.main_preferences);
+        
+        Preference weatherPreference = findPreference(WEATHER);
+        weatherPreference.setOnPreferenceClickListener(this);
+        weatherPreference.setOnPreferenceChangeListener(this);
+        Preference locationPreference = findPreference(LOCATION);
+        locationPreference.setOnPreferenceChangeListener(this);
+        
+        startUpdate();
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        String key = preference.getKey();
+        if (WEATHER.equals(key)) {
+            setProgressBarIndeterminateVisibility(true);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String key = preference.getKey();
+        if (WEATHER.equals(key)) {
+            //Log.d(TAG, String.valueOf(newValue));
+            setProgressBarIndeterminateVisibility(false);
+            return true;
+        }
+        if (LOCATION.equals(key)) {
+            startUpdate();
+            return true;
+        }
+        return false;
+    }
+    
+    void startUpdate() {
+        setProgressBarIndeterminateVisibility(true);
+        UpdateService.start(this, true);
     }
 
 }
