4b8d3c4f5a41d684b514ecffac7b7593a129d868
Denis Nelubin <dnelubin@gmail.com>
2011-01-15 02:15:37 +0600

    Changed notification icon.

--- misc/gen-status-icons.sh
+++ misc/gen-status-icons.sh
@@ -4,10 +4,11 @@
 t=60
 while [ $t -ge 10 ]
 do
-    convert misc/status-bar-icon.png \
-        -size 25x25 -gravity center -pointsize 11 \
-        -fill white -font Liberation-Sans-Bold \
-        -annotate +1+0 "-$t°" \
+    convert -size 25x25 xc:transparent \
+        -font Liberation-Sans-Bold \
+        -gravity center -pointsize 11 \
+        -stroke gray -strokewidth 5 -annotate 0 "-$t°" -blur 0x5 \
+        -fill white -stroke none -annotate 0 "-$t°" \
         res/drawable/temp_minus_$t.png
     #echo $t
     t=$(expr $t - 1)
@@ -17,30 +18,33 @@ done
 t=9
 while [ $t -ge 1 ]
 do
-    convert misc/status-bar-icon.png \
-        -size 25x25 -gravity center -pointsize 11 \
-        -fill white -font Liberation-Sans-Bold \
-        -annotate +1+0 "-$t°" \
+    convert -size 25x25 xc:transparent \
+        -font Liberation-Sans-Bold \
+        -gravity center -pointsize 11 \
+        -stroke gray -strokewidth 5 -annotate 0 "-$t°" -blur 0x5 \
+        -fill white -stroke none -annotate 0 "-$t°" \
         res/drawable/temp_minus_$t.png
     #echo $t
     t=$(expr $t - 1)
 done
 
 #0
-convert misc/status-bar-icon.png \
-        -size 25x25 -gravity center -pointsize 11 \
-        -fill white -font Liberation-Sans-Bold \
-        -annotate +3+0 "0°" \
+    convert -size 25x25 xc:transparent \
+        -font Liberation-Sans-Bold \
+        -gravity center -pointsize 11 \
+        -stroke gray -strokewidth 5 -annotate 0 "0°" -blur 0x5 \
+        -fill white -stroke none -annotate 0 "0°" \
         res/drawable/temp_0.png
 
 #1 - 9
 t=1
 while [ $t -le 9 ]
 do
-    convert misc/status-bar-icon.png \
-        -size 25x25 -gravity center -pointsize 11 \
-        -fill white -font Liberation-Sans-Bold \
-        -annotate +1+0 "+$t°" \
+    convert -size 25x25 xc:transparent \
+        -font Liberation-Sans-Bold \
+        -gravity center -pointsize 11 \
+        -stroke gray -strokewidth 5 -annotate 0 "+$t°" -blur 0x5 \
+        -fill white -stroke none -annotate 0 "+$t°" \
         res/drawable/temp_plus_$t.png
     #echo $t
     t=$(expr $t + 1)
@@ -50,10 +54,11 @@ done
 t=10
 while [ $t -le 99 ]
 do
-    convert misc/status-bar-icon.png \
-        -size 25x25 -gravity center -pointsize 11 \
-        -fill white -font Liberation-Sans-Bold \
-        -annotate +1+0 "+$t°" \
+    convert -size 25x25 xc:transparent \
+        -font Liberation-Sans-Bold \
+        -gravity center -pointsize 11 \
+        -stroke gray -strokewidth 5 -annotate 0 "+$t°" -blur 0x5 \
+        -fill white -stroke none -annotate 0 "+$t°" \
         res/drawable/temp_plus_$t.png
     #echo $t
     t=$(expr $t + 1)
@@ -63,10 +68,11 @@ done
 t=100
 while [ $t -le 130 ]
 do
-    convert misc/status-bar-icon.png \
-        -size 25x25 -gravity center -pointsize 11 \
-        -fill white -font Liberation-Sans-Bold \
-        -annotate +0+0 "+$t" \
+    convert -size 25x25 xc:transparent \
+        -font Liberation-Sans-Bold \
+        -gravity center -pointsize 11 \
+        -stroke gray -strokewidth 5 -annotate 0 "+$t" -blur 0x5 \
+        -fill white -stroke none -annotate 0 "+$t" \
         res/drawable/temp_plus_$t.png
     #echo $t
     t=$(expr $t + 1)
