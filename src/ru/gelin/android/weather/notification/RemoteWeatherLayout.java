5ef9216f2e41aa119b6ffdc0a87a3d561127b637
Denis Nelubin <dnelubin@gmail.com>
2011-01-17 01:34:57 +0600

    Fixing notification layout...

--- src/ru/gelin/android/weather/notification/RemoteWeatherLayout.java
+++ src/ru/gelin/android/weather/notification/RemoteWeatherLayout.java
@@ -21,6 +21,9 @@ public class RemoteWeatherLayout extends AbstractWeatherLayout {
     
     @Override
     void setText(int viewId, String text) {
+        if (skipView(viewId)) { //TODO: how to determine if the view is absent?
+            return;
+        }
         if (text == null) {
             views.setTextViewText(viewId, "");
             return;
@@ -29,7 +32,20 @@ public class RemoteWeatherLayout extends AbstractWeatherLayout {
     }
     
     void setVisibility(int viewId, int visibility) {
+        if (skipView(viewId)) { //TODO: how to determine if the view is absent?
+            return;
+        }
         views.setViewVisibility(viewId, visibility);
+    }
+    
+    boolean skipView(int viewId) {
+        switch (viewId) {
+        case R.id.update_time:
+        case R.id.location:
+            return true;
+        default:
+            return false;
+        }
     }
 
 }
