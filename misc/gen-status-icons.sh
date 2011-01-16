dd313baec333f24c4e988d8077cc045d70f1827b
Denis Nelubin <dnelubin@gmail.com>
2011-01-17 00:36:02 +0600

    Viva large icons!

--- misc/gen-status-icons.sh
+++ misc/gen-status-icons.sh
@@ -4,11 +4,11 @@
 t=60
 while [ $t -ge 10 ]
 do
-    convert -size 25x25 xc:transparent \
+    convert -size 200x25 xc:transparent \
         -font Liberation-Sans-Bold \
-        -gravity center -pointsize 11 \
-        -stroke gray -strokewidth 5 -annotate 0 "-$t°" -blur 0x5 \
+        -gravity center -pointsize 22 \
         -fill white -stroke none -annotate 0 "-$t°" \
+        -trim +repage -bordercolor transparent -border 1x5 \
         res/drawable/temp_minus_$t.png
     #echo $t
     t=$(expr $t - 1)
@@ -18,33 +18,33 @@ done
 t=9
 while [ $t -ge 1 ]
 do
-    convert -size 25x25 xc:transparent \
+    convert -size 200x25 xc:transparent \
         -font Liberation-Sans-Bold \
-        -gravity center -pointsize 11 \
-        -stroke gray -strokewidth 5 -annotate 0 "-$t°" -blur 0x5 \
+        -gravity center -pointsize 22 \
         -fill white -stroke none -annotate 0 "-$t°" \
+        -trim +repage -bordercolor transparent -border 1x5 \
         res/drawable/temp_minus_$t.png
     #echo $t
     t=$(expr $t - 1)
 done
 
 #0
-    convert -size 25x25 xc:transparent \
+    convert -size 200x25 xc:transparent \
         -font Liberation-Sans-Bold \
-        -gravity center -pointsize 11 \
-        -stroke gray -strokewidth 5 -annotate 0 "0°" -blur 0x5 \
+        -gravity center -pointsize 22 \
         -fill white -stroke none -annotate 0 "0°" \
+        -trim +repage -bordercolor transparent -border 1x5 \
         res/drawable/temp_0.png
 
 #1 - 9
 t=1
 while [ $t -le 9 ]
 do
-    convert -size 25x25 xc:transparent \
+    convert -size 200x25 xc:transparent \
         -font Liberation-Sans-Bold \
-        -gravity center -pointsize 11 \
-        -stroke gray -strokewidth 5 -annotate 0 "+$t°" -blur 0x5 \
+        -gravity center -pointsize 22 \
         -fill white -stroke none -annotate 0 "+$t°" \
+        -trim +repage -bordercolor transparent -border 1x5 \
         res/drawable/temp_plus_$t.png
     #echo $t
     t=$(expr $t + 1)
@@ -54,11 +54,11 @@ done
 t=10
 while [ $t -le 99 ]
 do
-    convert -size 25x25 xc:transparent \
+    convert -size 200x25 xc:transparent \
         -font Liberation-Sans-Bold \
-        -gravity center -pointsize 11 \
-        -stroke gray -strokewidth 5 -annotate 0 "+$t°" -blur 0x5 \
+        -gravity center -pointsize 22 \
         -fill white -stroke none -annotate 0 "+$t°" \
+        -trim +repage -bordercolor transparent -border 1x5 \
         res/drawable/temp_plus_$t.png
     #echo $t
     t=$(expr $t + 1)
@@ -68,11 +68,11 @@ done
 t=100
 while [ $t -le 130 ]
 do
-    convert -size 25x25 xc:transparent \
+    convert -size 200x25 xc:transparent \
         -font Liberation-Sans-Bold \
-        -gravity center -pointsize 11 \
-        -stroke gray -strokewidth 5 -annotate 0 "+$t" -blur 0x5 \
-        -fill white -stroke none -annotate 0 "+$t" \
+        -gravity center -pointsize 22 \
+        -fill white -stroke none -annotate 0 "+$t°" \
+        -trim +repage -bordercolor transparent -border 1x5 \
         res/drawable/temp_plus_$t.png
     #echo $t
     t=$(expr $t + 1)
