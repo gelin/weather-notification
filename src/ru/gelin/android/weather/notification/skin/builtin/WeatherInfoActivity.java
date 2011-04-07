330cf4aa933c753d4f8414d6d5506a8833d0065c
Denis Nelubin <dnelubin@gmail.com>
2011-04-08 01:01:21 +0700

    Improved weather info layout.

--- src/ru/gelin/android/weather/notification/skin/builtin/WeatherInfoActivity.java
+++ src/ru/gelin/android/weather/notification/skin/builtin/WeatherInfoActivity.java
@@ -22,7 +22,6 @@
 
 package ru.gelin.android.weather.notification.skin.builtin;
 
-import static ru.gelin.android.weather.notification.Tag.TAG;
 import ru.gelin.android.weather.Weather;
 import ru.gelin.android.weather.notification.R;
 import ru.gelin.android.weather.notification.WeatherLayout;
@@ -32,20 +31,17 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 public class WeatherInfoActivity extends Activity {
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather_info);
     }
     
     @Override
     protected void onResume() {
-        Log.d(TAG, "onResume");
         super.onResume();
         WeatherStorage storage = new WeatherStorage(this);
         WeatherLayout layout = new WeatherLayout(this, findViewById(R.id.weather_info));
@@ -59,7 +55,7 @@ public class WeatherInfoActivity extends Activity {
     protected static PendingIntent getPendingIntent(Context context) {
         Intent intent = new Intent(context, WeatherInfoActivity.class);
         //intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        //intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         return PendingIntent.getActivity(context, 0, intent, 0);
     }
 
