552560738422c7e502e9dd03f4f8641fb7fac561
Denis Nelubin <dnelubin@gmail.com>
2011-05-08 20:21:14 +0700

    Adding SkinManager.

---
+++ src/ru/gelin/android/weather/notification/skin/SkinInfo.java
@@ -0,0 +1,58 @@
+/*
+ *  Android Weather Notification.
+ *  Copyright (C) 2010  Denis Nelubin aka Gelin
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *  
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  http://gelin.ru
+ *  mailto:den@gelin.ru
+ */
+
+package ru.gelin.android.weather.notification.skin;
+
+/**
+ * 	Information about skin
+ */
+public class SkinInfo {
+
+	String name;
+	boolean enabled;
+	String broadcastReceiverClass;
+	String configActivityClass;
+	
+	SkinInfo(String name, boolean enabled, String broadcastReceiverClass, String configActivityClass) {
+		this.name = name;
+		this.enabled = enabled;
+		this.broadcastReceiverClass = broadcastReceiverClass;
+		this.configActivityClass = configActivityClass;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public String getBroadcastReceiverClass() {
+		return broadcastReceiverClass;
+	}
+
+	public String getConfigActivityClass() {
+		return configActivityClass;
+	}
+	
+}
