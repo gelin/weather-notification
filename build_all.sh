4940555d3886898413d3d787c75a8a8741ff9cdb
Denis Nelubin <dnelubin@gmail.com>
2011-05-20 02:00:07 +0700

    Set up command line build.

---
+++ build_all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+TARGETS=$*
+
+ant $TARGETS
+
+build_skin() {
+    BASE_DIR=$(pwd)
+    cd skins/$1
+    ant $TARGETS
+    cd $BASE_DIR
+}
+
+build_skin black-text
+build_skin white-text
