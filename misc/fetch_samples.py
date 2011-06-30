d748f14f94b9d1fa0cebadd4823bea596d99487e
Denis Nelubin <dnelubin@gmail.com>
2011-07-01 02:13:04 +0700

    Read weather info in all languages.

--- misc/fetch_samples.py
+++ misc/fetch_samples.py
@@ -4,6 +4,7 @@ import sys
 import os
 import os.path
 import ConfigParser
+import csv
 import httplib
 
 cwd = os.path.abspath((os.path.dirname(sys.argv[0])))
@@ -16,8 +17,14 @@ config.read(config_file)
 samples_dir = os.path.abspath(os.path.join(cwd, config.get('dirs', 'samples_dir')))
 if not os.access(samples_dir, os.W_OK):
     os.mkdirs(samples_dir)
+    
+lang_file = os.path.abspath(os.path.join(cwd, config.get('languages', 'lang_file')))
+lang_reader = csv.reader(open(lang_file), delimiter='|')
 
-for lang in config.options('languages'):
+for row in lang_reader:
+    lang = row[2]
+    if not lang:
+        continue
     sample_file = os.path.join(samples_dir, ('%s.xml' % lang))
     sample_path = '/ig/api?weather=Omsk&hl=%s' % lang
     print '%s -> %s' % (sample_path, sample_file)
