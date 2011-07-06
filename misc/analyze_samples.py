caaec49e248cc3931afb44a6888daca430f56445
Denis Nelubin <dnelubin@gmail.com>
2011-07-07 02:53:49 +0700

    Analyzed encodings...

--- misc/analyze_samples.py
+++ misc/analyze_samples.py
@@ -4,7 +4,7 @@ import sys
 import os
 import os.path
 import ConfigParser
-import xml.dom.minidom
+from xml.etree import ElementTree
 
 cwd = os.path.abspath((os.path.dirname(sys.argv[0])))
 
@@ -14,12 +14,19 @@ config = ConfigParser.RawConfigParser()
 config.read(config_file)
 
 samples_dir = os.path.abspath(os.path.join(cwd, config.get('dirs', 'samples_dir')))
-    
+
 for file in os.listdir(samples_dir):
-    lang = os.path.splitext(file)[0]
-    print lang
-    dom = xml.dom.minidom.parse(os.path.abspath(os.path.join(samples_dir, file)))
-    unit = dom.getElementsByTagName('unit_system').getAttribute('data')
-    wind = dom.getElementsByTagName('wind_condition').getAttribute('data')
-    humidity = dom.getElementsByTagName('humidity').getAttribute('data')
-    print '%s\t%s\t%s\t%s' % (lang, unit, wind, humidity)
+    (lang, encoding) = file.split('.')[0:2]
+    #print lang
+    #content = unicode(open(os.path.abspath(os.path.join(samples_dir, file))).read(), encoding)
+    #print content
+    try:
+        xml = ElementTree.parse(os.path.abspath(os.path.join(samples_dir, file)),
+                            ElementTree.XMLParser(encoding=encoding))
+    except Exception as e:
+        print file, e
+        continue
+    unit = next(xml.iter('unit_system')).get('data')
+    wind = next(xml.iter('wind_condition')).get('data')
+    humidity = next(xml.iter('humidity')).get('data')
+    print ('%s\t%s\t%s\t%s' % (lang, unit, wind, humidity)).encode('utf-8')
