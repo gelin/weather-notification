68b961342c1917b43e1f88cf9b5827ca298a7c8c
Denis Nelubin <dnelubin@gmail.com>
2011-11-04 15:26:51 +0600

    Added a test for weather storage.

--- tests/src/ru/gelin/android/weather/notification/WeatherStorageTest.java
+++ tests/src/ru/gelin/android/weather/notification/WeatherStorageTest.java
@@ -27,15 +27,71 @@ import java.io.InputStreamReader;
 import java.util.Calendar;
 import java.util.TimeZone;
 
-import ru.gelin.android.weather.v_0_2.Temperature;
-import ru.gelin.android.weather.v_0_2.UnitSystem;
-import ru.gelin.android.weather.v_0_2.WeatherCondition;
+import ru.gelin.android.weather.Humidity;
+import ru.gelin.android.weather.Temperature;
+import ru.gelin.android.weather.TemperatureUnit;
+import ru.gelin.android.weather.Weather;
+import ru.gelin.android.weather.WeatherCondition;
+import ru.gelin.android.weather.Wind;
+import ru.gelin.android.weather.WindDirection;
+import ru.gelin.android.weather.WindSpeedUnit;
 import ru.gelin.android.weather.google.GoogleWeather;
-import ru.gelin.android.weather.v_0_2.Weather;
 import android.test.AndroidTestCase;
 
 public class WeatherStorageTest extends AndroidTestCase {
 
+    public void testSaveLoad() throws Exception {
+        InputStream xml1 = getClass().getResourceAsStream("google_weather_api_en.xml");
+        InputStream xml2 = getClass().getResourceAsStream("google_weather_api_en.xml");
+        GoogleWeather weatherToSave = GoogleWeather.parse(
+                new InputStreamReader(xml1, "UTF-8"), new InputStreamReader(xml2, "UTF-8"));
+        WeatherStorage storage = new WeatherStorage(getContext());
+        storage.save(weatherToSave);
+        Weather weather = storage.load();
+        
+        assertEquals("Omsk, Omsk Oblast", weather.getLocation().getText());
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.set(2010, Calendar.DECEMBER, 28, 6, 0, 0);
+        assertEquals(calendar.getTime(), weather.getTime());
+        assertEquals(4, weather.getConditions().size());
+        
+        WeatherCondition condition0 = weather.getConditions().get(0);
+        assertEquals("Clear", condition0.getConditionText());
+        Temperature temp0 = condition0.getTemperature(TemperatureUnit.F);
+        assertEquals(-11, temp0.getCurrent());
+        assertEquals(-10, temp0.getLow());
+        assertEquals(-4, temp0.getHigh());
+        Humidity humidity = condition0.getHumidity();
+        assertEquals("Humidity: 66%", humidity.getText());
+        assertEquals(66, humidity.getValue());
+        Wind wind = condition0.getWind(WindSpeedUnit.MPH);
+        assertEquals("Wind: SW at 2 mph", wind.getText());
+        assertEquals(WindDirection.SW, wind.getDirection());
+        assertEquals(2, wind.getSpeed());
+        
+        WeatherCondition condition1 = weather.getConditions().get(1);
+        assertEquals("Snow Showers", condition1.getConditionText());
+        Temperature temp1 = condition1.getTemperature(TemperatureUnit.F);
+        assertEquals(7, temp1.getCurrent());
+        assertEquals(-7, temp1.getLow());
+        assertEquals(20, temp1.getHigh());
+        
+        WeatherCondition condition2 = weather.getConditions().get(2);
+        assertEquals("Partly Sunny", condition2.getConditionText());
+        Temperature temp2 = condition2.getTemperature(TemperatureUnit.F);
+        assertEquals(-10, temp2.getCurrent());
+        assertEquals(-14, temp2.getLow());
+        assertEquals(-6, temp2.getHigh());
+        
+        WeatherCondition condition3 = weather.getConditions().get(3);
+        assertEquals("Partly Sunny", condition3.getConditionText());
+        Temperature temp3 = condition3.getTemperature(TemperatureUnit.F);
+        assertEquals(-22, temp3.getCurrent());
+        assertEquals(-29, temp3.getLow());
+        assertEquals(-15, temp3.getHigh());
+    }
+    
     public void testBackwardCompatibility() throws Exception {
         InputStream xml1 = getClass().getResourceAsStream("google_weather_api_en.xml");
         InputStream xml2 = getClass().getResourceAsStream("google_weather_api_en.xml");
@@ -46,42 +102,46 @@ public class WeatherStorageTest extends AndroidTestCase {
         
         ru.gelin.android.weather.v_0_2.notification.WeatherStorage storage = 
             new ru.gelin.android.weather.v_0_2.notification.WeatherStorage(getContext());
-        Weather weather = storage.load();
+        ru.gelin.android.weather.v_0_2.Weather weather = storage.load();
         
         assertEquals("Omsk, Omsk Oblast", weather.getLocation().getText());
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
         calendar.set(Calendar.MILLISECOND, 0);
         calendar.set(2010, Calendar.DECEMBER, 28, 6, 0, 0);
         assertEquals(calendar.getTime(), weather.getTime());
-        assertEquals(UnitSystem.US, weather.getUnitSystem());
+        assertEquals(ru.gelin.android.weather.v_0_2.UnitSystem.US, weather.getUnitSystem());
         assertEquals(4, weather.getConditions().size());
         
-        WeatherCondition condition0 = weather.getConditions().get(0);
+        ru.gelin.android.weather.v_0_2.WeatherCondition condition0 = weather.getConditions().get(0);
         assertEquals("Clear", condition0.getConditionText());
-        Temperature temp0 = condition0.getTemperature(UnitSystem.US);
+        ru.gelin.android.weather.v_0_2.Temperature temp0 = 
+            condition0.getTemperature(ru.gelin.android.weather.v_0_2.UnitSystem.US);
         assertEquals(-11, temp0.getCurrent());
         assertEquals(-10, temp0.getLow());
         assertEquals(-4, temp0.getHigh());
         assertEquals("Humidity: 66%", condition0.getHumidityText());
         assertEquals("Wind: SW at 2 mph", condition0.getWindText());
         
-        WeatherCondition condition1 = weather.getConditions().get(1);
+        ru.gelin.android.weather.v_0_2.WeatherCondition condition1 = weather.getConditions().get(1);
         assertEquals("Snow Showers", condition1.getConditionText());
-        Temperature temp1 = condition1.getTemperature(UnitSystem.US);
+        ru.gelin.android.weather.v_0_2.Temperature temp1 = 
+            condition1.getTemperature(ru.gelin.android.weather.v_0_2.UnitSystem.US);
         assertEquals(7, temp1.getCurrent());
         assertEquals(-7, temp1.getLow());
         assertEquals(20, temp1.getHigh());
         
-        WeatherCondition condition2 = weather.getConditions().get(2);
+        ru.gelin.android.weather.v_0_2.WeatherCondition condition2 = weather.getConditions().get(2);
         assertEquals("Partly Sunny", condition2.getConditionText());
-        Temperature temp2 = condition2.getTemperature(UnitSystem.US);
+        ru.gelin.android.weather.v_0_2.Temperature temp2 = 
+            condition2.getTemperature(ru.gelin.android.weather.v_0_2.UnitSystem.US);
         assertEquals(-10, temp2.getCurrent());
         assertEquals(-14, temp2.getLow());
         assertEquals(-6, temp2.getHigh());
         
-        WeatherCondition condition3 = weather.getConditions().get(3);
+        ru.gelin.android.weather.v_0_2.WeatherCondition condition3 = weather.getConditions().get(3);
         assertEquals("Partly Sunny", condition3.getConditionText());
-        Temperature temp3 = condition3.getTemperature(UnitSystem.US);
+        ru.gelin.android.weather.v_0_2.Temperature temp3 = 
+            condition3.getTemperature(ru.gelin.android.weather.v_0_2.UnitSystem.US);
         assertEquals(-22, temp3.getCurrent());
         assertEquals(-29, temp3.getLow());
         assertEquals(-15, temp3.getHigh());
