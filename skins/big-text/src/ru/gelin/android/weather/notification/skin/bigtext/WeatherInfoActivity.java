fc8fa6a189febd4e887f297dd0ff82942532aa6d
Denis Nelubin <dnelubin@gmail.com>
2012-07-29 13:52:49 +0700

    Added new skin.

---
+++ skins/big-text/src/ru/gelin/android/weather/notification/skin/bigtext/WeatherInfoActivity.java
@@ -0,0 +1,34 @@
+/*
+ *  Android Weather Notification.
+ *  Copyright (C) 2010  Denis Nelubin aka Gelin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  http://gelin.ru
+ *  mailto:den@gelin.ru
+ */
+
+package ru.gelin.android.weather.notification.skin.bigtext;
+
+import ru.gelin.android.weather.notification.skin.impl.BaseWeatherInfoActivity;
+
+/**
+ *  Silently extends the basic weather info activity.
+ *  Adds no more functionality.
+ *  Used to declare a new class name to be inserted into manifest.
+ */
+public class WeatherInfoActivity extends BaseWeatherInfoActivity {
+
+}
