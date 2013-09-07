09f232893daa7b7781b81960d2092a36d0d13f0e
Denis Nelubin <dnelubin@gmail.com>
2013-09-07 22:02:16 +0700

    Trying to restore notification after the skin update.

---
+++ core/src/ru/gelin/android/weather/notification/app/PackageChangedReceiver.java
@@ -0,0 +1,41 @@
+/*
+ *  Android Weather Notification.
+ *  Copyright (C) 2010  Denis Nelubin aka Gelin
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  http://gelin.ru
+ *  mailto:den@gelin.ru
+ */
+
+package ru.gelin.android.weather.notification.app;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import ru.gelin.android.weather.notification.AppUtils;
+
+/**
+ *  Broadcast receiver which receives event about new package is updated.
+ *  Starts UpdateService to process possible skin update.
+ */
+public class PackageChangedReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive (Context context, Intent intent) {
+        AppUtils.startUpdateService(context);
+    }
+
+}
