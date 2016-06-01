d36b1c506e218276abcbccb94ee4d5ef78ca1ed6
Denis Nelubin <dnelubin@gmail.com>
2016-06-01 13:59:28 +0600

    Updated copying comment to GPLv3+ in all source files.

--- libs/libweatherskin/src/ru/gelin/android/weather/notification/skin/WeatherNotificationManager.java
+++ libs/libweatherskin/src/ru/gelin/android/weather/notification/skin/WeatherNotificationManager.java
@@ -1,23 +1,20 @@
 /*
- *  Android Weather Notification.
- *  Copyright (C) 2010  Denis Nelubin aka Gelin
+ * Copyright 2010—2016 Denis Nelubin and others.
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License as published by the Free Software Foundation; either
- *  version 2.1 of the License, or (at your option) any later version.
- *  
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * This file is part of Weather Notification.
  *
- *  http://gelin.ru
- *  mailto:den@gelin.ru
+ * Weather Notification is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Weather Notification is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Weather Notification.  If not, see http://www.gnu.org/licenses/.
  */
 
 package ru.gelin.android.weather.notification.skin;
